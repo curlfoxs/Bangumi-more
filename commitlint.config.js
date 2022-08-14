1646ecd364e1028c0782e374c2fdb90a5b232f5e
wullic <1564405657@qq.com>
2022-08-14 18:15:55 +0800

    style(commitlint): add new type chore

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,20 @@
-module.exports = { extends: ['@commitlint/config-angular'] };
+const types = [
+	'build',
+	'ci',
+	'docs',
+	'feat',
+	'fix',
+	'perf',
+	'refactor',
+	'revert',
+	'style',
+	'test',
+    'chore'
+];
+
+module.exports = { 
+    extends: ['@commitlint/config-angular'] ,
+    rules: {
+        "type-enum": [2, 'always', types],  
+    },
+};
