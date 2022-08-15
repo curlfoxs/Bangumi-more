368dabff9e971f6a03f5f509deb6632483fed839
wullic <1564405657@qq.com>
2022-08-16 05:36:00 +0800

    build(webpack): web dev server

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,35 @@ var path = require('path');
 
 module.exports = {
     // Change to your "entry-point".
-    entry: './src/index.ts',
+    entry: './src/index.ts', // The entry field tells Webpack where to start looking for modules to bundle
+    watch: true,
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bangumi-bbcode-preview.bundle.user.js'
     },
-    resolve: {
+    resolve: { // The resolve.extensions field tells Webpack what file types to look for in which order during module resolution.
         extensions: ['.ts', '.tsx', '.js', '.json']
     },
-    module: {
+    module: { // The module field tells Webpack how different modules will be treated.
         rules: [{
             // Include ts, tsx, js, and jsx files.
             test: /\.(ts|js)x?$/,
             exclude: /node_modules/,
             loader: 'babel-loader',
         }],
-    }
+    },
+    // The output field tells Webpack where to bundle our code. In our project, this is the file called bundle.js in the dist folder.
+    devServer: { // The devServer field configures the Webpack development server.
+        // We are telling it that the root of the web server is the dist folder, and to serve files on port 4000.
+        static: path.join(__dirname, "dist"),
+        compress: true,
+        port: 4000,
+        devMiddleware: {
+            index: true,
+            mimeTypes: { phtml: 'text/html' },
+            // publicPath: '/publicPathForDevServe',
+            serverSideRender: true,
+            writeToDisk: true,
+        },
+    },
 };
