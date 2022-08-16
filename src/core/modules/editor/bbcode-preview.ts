101472dec1dd8b003f5ebc9a6a260dbdb04797a4
wullic <1564405657@qq.com>
2022-08-16 23:29:52 +0800

    feat(preview): pass more dynamic cases

--- src/core/modules/editor/bbcode-preview.ts
+++ src/core/modules/editor/bbcode-preview.ts
@@ -1,49 +1,64 @@
+import { text } from "stream/consumers";
 import { transform, MarkLang } from "./outlined";
 
 function previewRun () {
-    const mkTextarea = document.querySelector("textarea") as HTMLTextAreaElement;
-    if (mkTextarea) {
-        mkTextarea.addEventListener("focus", delayAddPreview);
+    const l = document.querySelector("#comment_list");
+    l?.addEventListener("click", function f (evt: Event) {
+        const textarea = (evt.currentTarget as HTMLElement).querySelector("textarea.sub_reply");
+        if (textarea && textarea == evt.target && textarea.nextElementSibling.hasAttribute("class")) { // Check exist and no preivew-html div (created by us with no "class" attribute)
+            setTimeout(addPreview, 300, textarea);
+        }
+    });
+
+    const selectors = ["textarea#content", "textarea#comment", "textarea#newbio", "textarea#msg_body", "textarea#summary"];
+    selectors.forEach((s: string) => {
+        const mkTextarea = document.querySelector(s) as HTMLTextAreaElement;
+        if (mkTextarea) {
+            mkTextarea.addEventListener("focus", delayAddPreview);
+        }
+    });
+
+    function delayAddPreview (evt: Event) {
+        setTimeout(addPreview, 300, evt.currentTarget);
+        evt.currentTarget.removeEventListener("focus", delayAddPreview);
     }
 
-    function addPreview () {
-        const mkHeader = document.querySelector(".markItUpHeader") as HTMLElement;
-        if (mkHeader && mkTextarea) {
+    function addPreview (textarea: HTMLTextAreaElement) {
+        const mkHeader = textarea.previousElementSibling as HTMLElement;
+        if (mkHeader) {
+            // 1. Add div element to display preview-html
+            const div = document.createElement("div");
+            div.style.display = "none";
+            textarea.after(div);
+
+            // 2. Add a preview button
             const s = document.createElement("li");
             const previewBtn = document.createElement("li");
             const a = document.createElement("a");
-            const div = document.createElement("div");
-            div.style.display = "none";
-            mkTextarea.after(div);
-
-            s.setAttribute("class", "markItUpSeparator");
-            s.textContent = "---------------";
             a.href = "";
             a.textContent = "预览";
             a.setAttribute("title", "BBCode预览");
             previewBtn.append(a);
+            s.setAttribute("class", "markItUpSeparator");
+            s.textContent = "---------------";
             previewBtn.setAttribute("class", "markItUpButton markItUpButton16 preview");
             (mkHeader.firstChild as HTMLElement).append(s, previewBtn);
 
+            // 3. Add EventListener to preview button
             previewBtn.onclick = () => { return false; };
             previewBtn.addEventListener("click", evt => {
-                if (mkTextarea.style.display != "none") {
-                    const src = mkTextarea.value;
+                if (textarea.style.display != "none") {
+                    const src = textarea.value;
                     const dest = transform(src, MarkLang.BB, MarkLang.HTML);
                     div.innerHTML = dest.trim();
-                    mkTextarea.style.display = "none";
+                    textarea.style.display = "none";
                     div.style.display = "block";
                 } else {
-                    mkTextarea.style.display = "block";
+                    textarea.style.display = "block";
                     div.style.display = "none";
                 }
             });
         }
-    }
-
-    function delayAddPreview () {
-        setTimeout(addPreview, 300);
-        mkTextarea.removeEventListener("focus", delayAddPreview);
     }
 }
 
